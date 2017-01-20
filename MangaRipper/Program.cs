ec72273ab08882de62c5bfa057dde4ec8dc56f67
pnd <pnd@automic.com>
2017-01-20 11:15:54 +0700

    Implement global exception handling

--- MangaRipper/Program.cs
+++ MangaRipper/Program.cs
@@ -1,21 +1,34 @@
 ﻿using MangaRipper.Core.Providers;
 using System;
 using System.Windows.Forms;
+using NLog;
 
 namespace MangaRipper
 {
     static class Program
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Logger.Info("> Main()");
+            var appDomain = AppDomain.CurrentDomain;
+            appDomain.UnhandledException += AppDomain_UnhandledException;
             FrameworkProvider.Init("Plugins");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+            Logger.Info("< Main()");
+        }
+
+        private static void AppDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = (Exception)e.ExceptionObject;
+            Logger.Fatal(ex, "Unhandled Exception");
         }
     }
 }
