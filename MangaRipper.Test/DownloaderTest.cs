45d17c0590161562794c1aea4916f5599484b419
phuong.nguyen5 <phuong.nguyen5@niteco.se>
2017-11-28 14:15:33 +0700

    Add tests

---
+++ MangaRipper.Test/DownloaderTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MangaRipper.Core.Services;
+
+namespace MangaRipper.Test
+{
+    [TestClass]
+    public class DownloaderTest
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestUrl()
+        {
+            var downloader = new Downloader();
+        }
+    }
+}
