7407bb1910e98a0e8e4882fa9b563de6543dffa7
Gambit <rusnigdrag@gmail.com>
2018-04-15 17:51:40 +0600

    Add Chapter search for NHentai website

---
+++ MangaRipper.Plugin.NHentai/NHentai.cs
@@ -0,0 +1,108 @@
+﻿using MangaRipper.Core;
+using MangaRipper.Core.Interfaces;
+using MangaRipper.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MangaRipper.Plugin.NHentai
+{
+    public class NHentai : IMangaService
+    {
+        private readonly ILogger Logger;
+        private readonly IDownloader downloader;
+        private readonly IXPathSelector selector;
+        private readonly IRetry retry;
+        private readonly string SiteUrl = "https://nhentai.net";
+
+        public NHentai(ILogger myLogger, IDownloader downloader, IXPathSelector selector, IRetry retry)
+        {
+            Logger = myLogger;
+            this.downloader = downloader;
+            this.selector = selector;
+            this.retry = retry;
+        }
+
+        public SiteInformation GetInformation()
+        {
+            return new SiteInformation(nameof(NHentai), SiteUrl, "English");
+        }
+
+        public bool Of(string link)
+        {
+            var uri = new Uri(link);
+            return uri.Host.Equals("nhentai.net");
+        }
+
+        public async Task<IEnumerable<Chapter>> FindChapters(string manga, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            Logger.Info($@"> FindChapters(): {manga}");
+            progress.Report(0);
+
+            // find all chapters in a manga
+            IEnumerable<Chapter> chaps = await retry.DoAsync(() =>
+            {
+                return DownloadAndParseChapters(manga, cancellationToken);
+            }, TimeSpan.FromSeconds(3));
+            progress.Report(100);
+
+            return chaps;
+        }
+
+        public async Task<IEnumerable<string>> FindImages(Chapter chapter, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Private methods
+
+        private async Task<IEnumerable<Chapter>> DownloadAndParseChapters(string manga, CancellationToken cancellationToken)
+        {
+            string input = await downloader.DownloadStringAsync(manga, cancellationToken);
+
+            // In case if you open the chapter itself
+            var title = selector.Select(input, "//div[contains(@id,'content')]//h1").InnerHtml;
+
+            // If you open the page with number of chapters (artist/character/Group/Tags)
+            if (title.Contains("<span"))
+            {
+                // Title consist of three parts. e.g. [Character] [nobuna oda] [(6)]
+                // Take the first two
+                var titleType = selector.Select(input, "//div[contains(@id,'content')]//h1//span").InnerHtml;
+                var titleName = selector.Select(input, "//div[contains(@id,'content')]//h1//span[2]").InnerHtml;
+                title = titleType + " - " + titleName;
+
+                var chaps = selector.SelectMany(input, "//div[contains(@class,'container')]//a")
+                .Select(n =>
+                {
+                    string name = NameResolver(n.InnerHtml);
+                    string url = SiteUrl + n.Attributes["href"];
+                    return new Chapter(name, url) { Manga = title };
+                });
+
+                return chaps;
+            }
+
+            // We have only one chapter
+            var chap = new Chapter(title, manga) { Manga = title };
+
+            return new List<Chapter>() { chap };
+        }
+
+        /// <summary>
+        /// Get the name from the "<div class="caption">" tag
+        /// We have to use this method as we can't use "selector" inside "selector" - "startindex" out of range exception
+        /// </summary>
+        /// <param name="text">Text with html tags</param>
+        /// <returns></returns>
+        private string NameResolver(string text)
+        {
+            var htmlAdapter = new HtmlAtilityPackAdapter();
+            return htmlAdapter.Select(text, "//div[contains(@class,'caption')]").InnerHtml;
+        }
+
+        #endregion
+    }
+}
