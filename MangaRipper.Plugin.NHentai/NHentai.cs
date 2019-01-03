ce013d3c6fe60ee551df6152abc6cccfbc3e7c05
Gambit <17333840+GambitKZ@users.noreply.github.com>
2019-01-04 01:04:16 +0600

    Add test for NHentai

--- MangaRipper.Plugin.NHentai/NHentai.cs
+++ MangaRipper.Plugin.NHentai/NHentai.cs
@@ -4,7 +4,6 @@ using MangaRipper.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
